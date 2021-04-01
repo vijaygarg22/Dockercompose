84803d6ce76e302d5ff81a3710a9c5415c8d6da0
vijaygarg22 <vijaygarg22@gmail.com>
2021-04-01 10:20:39 +0530

    First commit

---
+++ phpdata/index.php
@@ -0,0 +1,7 @@
+<?php
+    echo '<h1>Hello everyone Hope you are fine!</h1>';
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?>
